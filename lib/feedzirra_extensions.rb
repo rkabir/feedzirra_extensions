4703d73c2ac8b4547cd461eed4fdbf344b21d506
Alvin Liang <ayliang@gmail.com>
2011-02-08 23:19:56 -0500

    now use generic parser class after fetching a multifeed

--- lib/feedzirra_extensions.rb
+++ lib/feedzirra_extensions.rb
@@ -1,60 +1,67 @@
 require 'feedzirra'
+require 'nokogiri'
 
 # TODO: better namespacing
 
-module FeedzirraParserExtensions
-  # mix this into feed, or whatever else has an entries object
-  # TODO: Need to learn to copy feeds instead of modifying entries in place
-  
-  def find_all_by_string(string)
-    self.entries = self.entries.find_all { |entry|
-      # TODO: Should not consider any embedded HTML
-      # TODO: Should consider any other attributes you care about
-      entry.title.include?(string) || entry.content.include?(string)
-    }
-  end
-  
-  def reject_by_string(string)
-    self.entries = self.entries.reject { |entry|
-      # TODO: Should not consider any embedded HTML
-      # TODO: Should consider any other attributes you care about
-      entry.title.include?(string) || entry.content.include?(string)
-    }
-  end
-  
-  def find_all_by_author(author_name)
-    self.entries = self.entries.find_all { |entry| 
-      entry.author.include?(author_name)
-    }
-  end
-  
-  def reject_by_author(author_name)
-    self.entries = self.entries.reject { |entry|
-      entry.author.include?(author_name)
-    }
-  end
-  
-  def find_all_with_image
-    puts "leave only entries with images"
-  end
-  
-  def reject_with_image
-    puts "remove entries with images"
-  end
-  
-  def map_to_images
-    puts "map this feed to images"
-  end
-  
-  def to_rss
-    # TODO: Need to implement conversion back to RSS XML
-    # Convert the feed back to RSS so you can use it elsewhere?
-    # Ideally you'd cache this so maybe this should be at the Rails level
-    # Probably want to use a templating language here
-  end
-end
-
 module Feedzirra
+  module FeedzirraParserExtensions
+    # mix this into feed, or whatever else has an entries object
+
+    def find_all_by_string(string)
+      entries = self.entries.find_all { |entry|
+        # TODO: Should not consider any embedded HTML
+        # TODO: Should consider any other attributes you care about
+        entry.title.include?(string) || entry.content.include?(string)
+      }
+      return ::Feedzirra::Parser::GenericParser.new(self.title, self.url, entries)
+    end
+
+    def reject_by_string(string)
+      entries = self.entries.reject { |entry|
+        # TODO: Should not consider any embedded HTML
+        # TODO: Should consider any other attributes you care about
+        entry.title.include?(string) || entry.content.include?(string)
+      }
+      return ::Feedzirra::Parser::GenericParser.new(self.title, self.url, entries)
+    end
+
+    def find_all_by_author(author_name)
+      entries = self.entries.find_all { |entry| 
+        entry.author.include?(author_name)
+      }
+      return ::Feedzirra::Parser::GenericParser.new(self.title, self.url, entries)
+    end
+
+    def reject_by_author(author_name)
+      entries = self.entries.reject { |entry|
+        entry.author.include?(author_name)
+      }
+      return ::Feedzirra::Parser::GenericParser.new(self.title, self.url, entries)
+    end
+
+    def find_all_with_image
+      puts "leave only entries with images"
+      # parse as HTML using Nokogiri, then see if there are any img tags.
+    end
+
+    def reject_with_image
+      puts "remove entries with images"
+      # parse as HTML using Nokogiri, then see if there are any img tags.
+    end
+
+    def map_to_images
+      puts "map this feed to images"
+      # call find_all_with_image, then strip everything but the images
+    end
+
+    def to_rss
+      # TODO: Need to implement conversion back to RSS XML
+      # Convert the feed back to RSS so you can use it elsewhere?
+      # Ideally you'd cache this so maybe this should be at the Rails level
+      # Probably want to use a templating language here
+    end
+  end
+  
   class MergedFeed
     def self.fetch_and_parse(title, url, *feed_urls)
       # Create a new feed parser instance from the given feeds,
@@ -68,15 +75,20 @@ module Feedzirra
       end
       # Sort by date published
       entries.sort! { |x,y| y.published <=> x.published }
-      return MergedParser.new(title, url, entries)
+      return ::Feedzirra::Parser::GenericParser.new(title, url, entries)
     end
   end
 
   module Parser
-    class MergedParser
+    class GenericParser
       # Not really a parser, just an object that looks like a
       # Feedzirra::Parser::XX object
-      # title, url, entries
+
+      # TODO: what about etags, last modified, etc?
+      # TODO: if it's filtered, then checking for updates will be different
+      # include ::FeedZirra::FeedUtilities
+      # include ::FeedZirra::FeedzirraParserExtensions
+      include FeedUtilities
       include FeedzirraParserExtensions
       attr_accessor :url, :title, :entries
       def initialize(title, url, entries)
@@ -95,6 +107,6 @@ end
   Feedzirra::Parser::ITunesRSS, Feedzirra::Parser::RSS
 ].each do |klass|
   klass.class_eval do
-    include FeedzirraParserExtensions
+    include Feedzirra::FeedzirraParserExtensions
   end
 end
