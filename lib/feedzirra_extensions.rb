35bf01b4648f21a946853c7093cf52ea9311dc38
Ryan Kabir <rkabir@alum.mit.edu>
2011-03-03 17:58:11 -0400

    refactor filtering down into chunks

--- lib/feedzirra_extensions.rb
+++ lib/feedzirra_extensions.rb
@@ -6,47 +6,77 @@ module Feedzirra
   module FeedzirraParserExtensions
     # mix this into feed, or whatever else has an entries object
 
+    def match_author(name)
+      name = name.downcase || ""
+      self.entries.find_all do |entry|
+        author = entry.author.downcase || ""
+        author.include?(name)
+      end
+    end
+
+    def cleaned_content(entry)
+      title = entry.title.downcase || ""
+      summary = entry.summary.downcase || ""
+      content = entry.content.downcase || ""
+      return title, summary, content
+    end
+
+    def match_exact_string(match_string)
+      text = match_string.downcase || ""
+      entries.find_all do |entry|
+        title, summary, content = cleaned_content(entry)
+        title.include?(text) ||
+          summary.include?(text) ||
+          content.include?(text)
+      end
+    end
+
+    def match_any_string(token_string)
+      tokens = token_string.split
+      results = []
+      tokens.each do |token|
+        results << match_exact_string(token)
+      end
+      return results.flatten
+    end
+
+    def entries_with_images
+      entries.find_all do |entry|
+        begin
+          html = Nokogiri::HTML(entry.content)
+        rescue
+          return true
+        end
+        html.search("img").length > 0
+      end
+    end
+
+    def entries_with_links
+      entries.find_all do |entry|
+        begin
+          html = Nokogiri::HTML(entry.content)
+        rescue
+          return true
+        end
+        html.search("a[href]").length > 0
+      end
+    end
+
     def where_entries(options = {})
       return self if options == {}
       options = options.with_indifferent_access
       entries = self.entries
       if options['text']
-        entries = entries.find_all do |entry|
-          title = entry.title.downcase || ""
-          summary = entry.summary.downcase || ""
-          content = entry.content.downcase || ""
-          text = options['text'].downcase || ""
-          title.include?(text) ||
-            summary.include?(text) ||
-            content.include?(text)
-        end
+        entries = match_exact_string(options['text'])
       end
       if options['author']
-        entries = entries.find_all do |entry|
-          author = entry.author.downcase || ""
-          text = options['author'].downcase || ""
-          author.include?(text)
-        end
+        entries = match_author(name)
       end
       if options['has_image']
-        entries = entries.find_all do |entry|
-          begin
-            html = Nokogiri::HTML(entry.content)
-          rescue
-            return true
-          end
-          html.search("img").length > 0
-        end
+        entries = entries_with_images
       end
       if options['has_link']
-        entries = entries.find_all do |entry|
-          begin
-            html = Nokogiri::HTML(entry.content)
-          rescue
-            return true
-          end
-          links = html.search("a[href]").length > 0
-        end
+        entries = entries_with_links
       end
       if options['has_attachment']
         entries = entries.find_all do |entry|
