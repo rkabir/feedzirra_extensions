bbdfc21a96e48daed3a18279b3aefd8a036dcc3e
Alvin Liang <ayliang@gmail.com>
2011-03-21 18:42:54 -0400

    bug in entries_randomly

--- lib/feedzirra_extensions.rb
+++ lib/feedzirra_extensions.rb
@@ -169,7 +169,7 @@ module Feedzirra
 
     def entries_randomly(frequency, reject = false)
       frequency = 1 - frequency if reject
-      return entries if frequency >= 1
+      # return entries if frequency >= 1
       proc = Proc.new { |entry|
         Random.rand < frequency
       }
