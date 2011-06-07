16865b565d691a1ad80a548cff589379f19d4417
Alvin Liang <ayliang@gmail.com>
2011-06-06 21:16:45 -0400

    hey, there's a meaningful test now (it checks to see if the module is included)

--- spec/feedzirra/feedzirra_parser_extensions_spec.rb
+++ spec/feedzirra/feedzirra_parser_extensions_spec.rb
@@ -1,6 +1,24 @@
-require 'rubygems'
 require 'spec_helper'
 
 describe Feedzirra::FeedzirraParserExtensions do
-  pending "hey buddy, you should probably rename this file and start testing for real"
+  #before (:each) do
+    #@class = Class.new do
+    #  include Feedzirra::FeedzirraParserExtensions
+    #end
+  #end
+  # @feed = Feedzirra::Feed.parse(sample_atom_feed)
+  it "should include the extensions in parsed feeds" do
+    atom = Feedzirra::Feed.parse(sample_atom_feed)
+    itunes = Feedzirra::Feed.parse(sample_itunes_feed)
+    rdf = Feedzirra::Feed.parse(sample_rdf_feed)
+    rss = Feedzirra::Feed.parse(sample_rss_feed)
+    rss_feedburner = Feedzirra::Feed.parse(sample_rss_feed_burner_feed)
+    atom_feedburner = Feedzirra::Feed.parse(sample_feedburner_atom_feed)
+    wfw = Feedzirra::Feed.parse(sample_wfw_feed)
+    
+    feeds = [atom, itunes, rdf, rss, rss_feedburner, atom_feedburner, wfw]
+    feeds.each do |f|
+      f.class.include?(Feedzirra::FeedzirraParserExtensions).should be_true
+    end
+  end
 end
