6cb2f633d1fa551aa21d17cbebd2a08b49b535e0
Alvin Liang <ayliang@gmail.com>
2011-06-06 21:50:14 -0400

    test title filtering

--- spec/feedzirra/feedzirra_parser_extensions_spec.rb
+++ spec/feedzirra/feedzirra_parser_extensions_spec.rb
@@ -21,4 +21,34 @@ describe Feedzirra::FeedzirraParserExtensions do
       f.class.include?(Feedzirra::FeedzirraParserExtensions).should be_true
     end
   end
+  
+  describe "filtering by title" do
+    before (:each) do
+      @atom = Feedzirra::Feed.parse(sample_atom_feed)
+    end
+    
+    it "should have phrase in title" do
+      filtered = @atom.match_title("cloud computing")
+      filtered.size.should == 1
+    end
+    it "should have any word in title" do
+      filtered = @atom.match_title_any_word("mainframe job")
+      filtered.size.should == 2
+    end
+    it "should have all words in title" do
+      filtered = @atom.match_title_all_words("mainframe cloud")
+      filtered.size.should == 1
+    end
+  end
+  pending "should filter by author"
+  pending "should filter by keyword"
+  pending "should filter by text"
+  pending "should filter by images"
+  pending "should filter by links"
+  pending "should filter randomly"
+  
+  describe "calling the correct methods when using where and arguments" do
+  end
+  describe "calling the correct methods when using where_not and arguments" do
+  end
 end
